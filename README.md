5a531a0a29b10e31e3f4b2fc5843ae004bf3bc1f
Alex <alex@theblockchaininstitute.org>
2018-09-12 17:43:35 -0500

    updating with clarified form fields and readme

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ Just has a basic form for now.
 
 Basically the way it works is that each step of the form has a callback that it triggers to verify the data before proceeding to the next step. 
 
+Usually I store all the form fields and validate them as they continue, and then the last step is to call a serverside function named 'CheckCaptcha' which verified their captcha as a part of the payload. I've included a template of that function as well for reference.
+
+I'm not really sure what your data set will need to be like coming from the form but you should be able to follow the syntax. The main thing is that the angular controller has what's called a model, and anything in the model has to be declared as a sub-object of the `$scope` object.
+
+EX: 
+Declaration in the JS file:
+
+`$scope.test = 'Lou';`
+
+Then in the html you can write 
+
+`<p>{{test}}<p>`
+
+which will output 
+
+`<p>Lou<p>`
---
+++ checkCaptcha Template.js
@@ -0,0 +1,32 @@
+exports.checkCaptcha = function (req, res) {
+    var response = req.body.response
+    console.log(req.body);
+    console.log("recaptcha response", response)
+
+	    rp({
+	        uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
+	        method: 'POST',
+	        formData: {
+	            secret: '6Lef510UAAAAACOoGmTQWtzXB3Zuoc6-Dbu1LgN-',
+	            response: response
+	        },
+	        json: true
+	    }).then(result => {
+	    	// return { error : NOT_AUTHENTICATED };
+	        console.log("recaptcha result", result)
+	        if (result.success) {
+	        	console.log('success: true')
+	            sendMail(req.body, res);	        	
+
+
+	        } else {
+	        	console.log('success: false')
+	            return res.status(200).send("Recaptcha verification failed. Are you a robot?")
+	        }
+
+	    }).catch(reason => {
+	        console.log("Recaptcha request failure", reason)
+	        return ("Recaptcha request failed.")
+	    })
+
+}
